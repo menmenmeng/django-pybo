bb4c82ab1c2d14c1546dcb9a136340125b10da0b
menmenmeng <franksmh0409@gmail.com>
2021-09-15 14:57:23 +0900

    answer form modified

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from pybo.models import Question
+from pybo.models import Answer, Question
 
 
 class QuestionForm(forms.ModelForm):
@@ -12,5 +12,14 @@ class QuestionForm(forms.ModelForm):
         }
         labels = {
             'subject': '제목',
+            'content': '내용',
+        }
+
+# Answer Form - django 따라하기 아니고 내가 직접하는거
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
             'content': '내용',
         }
--- pybo/views.py
+++ pybo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 # from django.http import HttpResponse
 from django.utils import timezone
 from .models import Question
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 
@@ -35,13 +35,25 @@ def detail(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     """
     pybo 답변등록
     """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question.id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.question = question
+            answer.author = request.user
+            answer.create_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
 
 
 @login_required(login_url='common:login')
